157eeed287db57aaf746d9da7a0c35deceb905d5
albertov19 <64150856+albertov19@users.noreply.github.com>
2020-11-20 12:10:18 +0100

    Updated address

--- code-snippets/web3-tx/transaction.js
+++ code-snippets/web3-tx/transaction.js
@@ -4,7 +4,7 @@ const Web3 = require('web3');
 const privKey =
    '99b3c12287537e38c90a9219d4cb074a89a16e9cdb20bf85728ebd97c343e342'; // Genesis private key
 const addressFrom = '0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b';
-const addressTo = '0x12Cb274aAD8251C875c0bf6872b67d9983E53fDd'; // Change addressTo
+const addressTo = '0x44236223aB4291b93EEd10E4B511B37a398DEE55'; // Change addressTo
 const web3 = new Web3('http://localhost:9933');
 
 // Create transaction
