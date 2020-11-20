c4d484f80dbbad14031c6cda40de792e002b83ac
albertov19 <64150856+albertov19@users.noreply.github.com>
2020-11-20 12:10:02 +0100

    Updated address

--- code-snippets/web3-tx/balances.js
+++ code-snippets/web3-tx/balances.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 
 // Variables definition
 const addressFrom = '0x6Be02d1d3665660d22FF9624b7BE0551ee1Ac91b';
-const addressTo = '0xB90168C8CBcd351D069ffFdA7B71cd846924d551';
+const addressTo = '0x44236223aB4291b93EEd10E4B511B37a398DEE55';
 const web3 = new Web3('http://localhost:9933');
 
 // Balance call
