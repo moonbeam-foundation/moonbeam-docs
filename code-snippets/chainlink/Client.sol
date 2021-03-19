75219a519e2cabc351e412ec2467d62e79a07343
albertov19 <albertoviera@gmail.com>
2021-03-19 17:50:07 +0000

    Udpdated submodule

--- code-snippets/chainlink/Client.sol
+++ code-snippets/chainlink/Client.sol
@@ -11,24 +11,14 @@ contract Client is ChainlinkClient {
   uint256 public currentPrice;
   address public owner;
   
-  /**
-   * @notice Deploy with the address of the LINK token
-   * @dev Sets the LinkToken address for the imported LinkTokenInterface
-   * @param _link The address of the LINK token
-   */
+  // Deploy with the address of the LINK token
   constructor(address _link) public {
     // Set the address for the LINK token for the network
     setChainlinkToken(_link);
     owner = msg.sender;
   }
 
-  /**
-   * @notice Creates a Chainlink request with the job specification ID,
-   * @notice and sends it to the Oracle.
-   * @notice _oracle The address of the Oracle contract fixed top
-   * @notice _payment For this example the PAYMENT is set to zero
-   * @param _jobId The job spec ID that we want to call in string format 
-   */
+  // Creates Chainlink Request
   function requestPrice(address _oracle, string memory _jobId, uint256 _payment) 
     public
     onlyOwner
@@ -39,12 +29,7 @@ contract Client is ChainlinkClient {
     sendChainlinkRequestTo(_oracle, req, _payment);
   }
 
-  /**
-   * @notice Callback function called by the Oracle when it has resolved the request
-   * @dev Using recordChainlinkFulfillment to ensure only the requesting oracle can fulfill
-   * @param _requestId The request ID sent to the Oracle in the first place
-   * @param _price The result of the request (multiplied by a factor of 100)
-   */
+  // Callback function called by the Oracle when it has resolved the request
   function fulfill(bytes32 _requestId, uint256 _price)
     public
     recordChainlinkFulfillment(_requestId)
@@ -52,14 +37,7 @@ contract Client is ChainlinkClient {
     currentPrice = _price;
   }
   
-  /**
-   * @notice Allows the owner to cancel an unfulfilled request
-   * @param _requestId The request ID sent to the Oracle in the first place
-   * @param _payment The payment sent to the Oracle in the first place
-   * @param _callbackFunctionId The ID of the callback function that would 
-   * have been called by the Oracle to store the result of the request
-   * @param _expiration The time of expiration for the request
-   */
+  // Allows the owner to cancel an unfulfilled request
   function cancelRequest(
     bytes32 _requestId,
     uint256 _payment,
@@ -71,10 +49,7 @@ contract Client is ChainlinkClient {
     cancelChainlinkRequest(_requestId, _payment, _callbackFunctionId, _expiration);
   }
 
-  /**
-   * @notice Allows the owner to withdraw the LINK tokens in the contract
-   * @notice to the address calling this function
-   */
+  // Allows the owner to withdraw the LINK tokens in the contract to the address calling this function
   function withdrawLink()
     public
     onlyOwner
@@ -83,11 +58,7 @@ contract Client is ChainlinkClient {
     require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
   }
 
-  /**
-   * @notice Decodes an input string in a bytes32 word
-   * @param _source The input string
-   * @return result A bytes32 word of the decoded string
-   */
+  // Decodes an input string in a bytes32 word
   function stringToBytes32(string memory _source)
     private
     pure
@@ -105,9 +76,7 @@ contract Client is ChainlinkClient {
     return result;
   }
   
-  /**
-   * @dev Reverts if the sender is not the owner of the contract
-   */
+  // Reverts if the sender is not the owner of the contract
   modifier onlyOwner() {
     require(msg.sender == owner);
     _;
