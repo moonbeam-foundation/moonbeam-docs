06a4e769a04b54234192aef9c6b5237770be3e2c
eshaben <eshaben@icloud.com>
2021-04-21 10:08:21 -0600

    add a modal for error handling

--- js/connectMetaMask.js
+++ js/connectMetaMask.js
@@ -2,6 +2,10 @@ const provider = window.ethereum;
 
 /** Connect to Moonbase Alpha */
 const setupMoonbaseAlpha = async () => {
+    /** In case we need to throw an error, let's grab the error modal & error message */
+    const errorModalContainer = document.querySelector(".error-modal-container");
+    const errorMessage = document.querySelector(".error-message");
+
     if (provider) {
         try {
             await provider.request({ method: "eth_requestAccounts"});
@@ -22,8 +26,15 @@ const setupMoonbaseAlpha = async () => {
             })
         } catch(e) {
             console.error(e);
+            /** Code 4001 is user rejected, we don't need to notify the user if they rejected the request */
+            if (e.code !== 4001) {
+                errorModalContainer.style.display = "block";
+                errorMessage.innerHTML = e.message;
+            }
         }
     } else {
+        errorModalContainer.style.display = "block";
+        errorMessage.innerHTML = `It looks like MetaMask hasn't been installed. Please <a href="https://metamask.io/download.html" target="_blank" rel="noreferrer noopener">install MetaMask</a> and try again.`
         console.log("error!");
     }
 }
