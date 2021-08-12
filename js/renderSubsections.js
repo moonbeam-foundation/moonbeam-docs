644fa67b9132b6ce23113120df4e700d47ef308c
eshaben <eshaben@icloud.com>
2021-08-11 22:18:10 -0600

    update logic for generating section index pages

--- js/renderSubsections.js
+++ js/renderSubsections.js
@@ -1,53 +1,37 @@
 // Get the current page the user is on
-const pathName = window.location.pathname.split('/');
-const currentPage = pathName[pathName.length - 2];
+const pathname = window.location.pathname;
+const classname = pathname.replace('/', '.').replaceAll('/', '-');
 
-// Get the nav section for the page
-let sections;
-if (currentPage){
-  sections = document.querySelectorAll(`.${currentPage.toLowerCase()}`)
-}
+const section = document.querySelector(classname)
+console.log(section.children)
 
 // Get the div to append the subsection cards to
 const wrapper = document.querySelector(".subsection-wrapper");
 
-// Append the subsection cards
+// Append the cards
 const appendCards = (section) => {
-  for (let subsection of section.children) {
-    const subsectionContent = subsection.innerText.trim();
-    let subsectionName = subsectionContent.includes("\n") ? subsectionContent.split("\n")[0] : subsectionContent;
-    let subsectionLink = subsectionName.includes(' ') ? subsectionName.replaceAll(' ', '-').toLowerCase() : subsectionName.toLowerCase();
-    if (subsectionLink.includes('-&-')){
-      subsectionLink = subsectionLink.replace('-&-', '-');
-    }
+  const href = section.href;
+  const image = href.split('/').slice(3, -1).join("/");  
+  const title = section.innerText;
 
-    wrapper.innerHTML += `
-    <div class="card">
-      <a href="${window.location.href}${subsectionLink}/">
-        <h2 class="title">${subsectionName}</h2>
-        <img class="icon" src="/images/index-pages${window.location.pathname}${subsectionLink}.png">
-      </a>
-    </div>
-    `
-  }
-};
+  wrapper.innerHTML += `
+  <div class="card">
+    <a href=${href}>
+      <h2 class="title">${title}</h2>
+      <img class="icon" src="/images/index-pages/${image}.png">
+    </a>
+  </div>
+  `
+}
 
-if (wrapper) {
-  // If we have multiple sections with the same name, we need to find the correct section
-  if (sections.length > 1) {
-    for (let section of sections) {
-      // To determine the correct section, we have to look at the subsection items
-      const subsectionContentList = section.getElementsByClassName("md-nav__link");
-      for (let content of subsectionContentList) {
-        // If the href starts with "../", then the subsection is being displayed on another
-        // page and is not the subsection we are looking for
-        if (content.hasAttribute("href") && !content.attributes["href"].value.startsWith("../")){
-          appendCards(section);
-          break;
-        }
-      }
+if (section && section.children) {
+  for (subsection of section.children) {
+    // If it's a directory and is nested, we'll need to dig deeper to get the information for each nav item
+    if (subsection.classList.contains("md-nav__item--nested")){    
+      appendCards(subsection.children[1].children[1])  
+    } else {
+      appendCards(subsection.children[0])
     }
-  } else {
-    appendCards(sections[0]); // Default to the first element
   }
 }
+
