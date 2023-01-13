ab3aceccaa193bde02a6eb6e9bb4646c8fa34473
Jeremy Boetticher <me4jeremyboett@gmail.com>
2023-01-13 13:40:16 -0600

    Added MyToken.t.sol code

--- .snippets/code/tutorials/foundry-start-to-end/MyToken.t.sol
+++ .snippets/code/tutorials/foundry-start-to-end/MyToken.t.sol
@@ -1,1 +1,30 @@
-// TODO: after fuzzing tests
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../src/MyToken.sol";
+
+contract MyTokenTest is Test {
+    MyToken public token;
+
+    function setUp() public {
+        token = new MyToken(100);
+    }
+
+    function testConstructorMint() public {
+        assertEq(token.balanceOf(address(this)), 100);
+    }
+
+    function testMintOneEtherOrBelow(uint256 amountToMint) public {
+        vm.assume(amountToMint <= 1 ether);
+
+        token.mint(amountToMint, msg.sender);
+        assertEq(token.balanceOf(msg.sender), amountToMint);
+    }
+
+    function testFailMintAboveOneEther(uint256 amountToMint) public {
+        vm.assume(amountToMint > 1 ether);
+        
+        token.mint(amountToMint, msg.sender);
+    }
+}
