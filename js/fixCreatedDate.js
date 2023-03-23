86520a9ce28085d77d0a3e27d78b86e4eb579d03
Erin Shaben <eshaben@icloud.com>
2023-03-22 23:38:21 -0400

    add script to check updated and created dates

---
+++ js/fixCreatedDate.js
@@ -0,0 +1,14 @@
+const dateLabels = document.querySelectorAll('.git-revision-date-localized-plugin-date');
+
+const lastUpdate = dateLabels[0].innerHTML;
+const created = dateLabels[1].innerHTML;
+
+const lastUpdateDate = new Date(lastUpdate);
+const createdDate = new Date(created);
+
+// If the created date is after the last updated date,
+// default to using the last updated date
+if (lastUpdateDate < createdDate) {
+    dateLabels[1].innerHTML = lastUpdate;
+}
+
