f470d60b102acd66a726bf6d66ed6ff2ce7c76fa
Erin Shaben <eshaben@icloud.com>
2023-05-10 22:13:30 -0400

    clean up web3py guide and add note about solidity v0.8.20

--- .snippets/code/web3py-contract/deploy.py
+++ .snippets/code/web3py-contract/deploy.py
@@ -1,31 +1,38 @@
+# 1. Add imports
 from compile import abi, bytecode
 from web3 import Web3
 
+# 2. Add the Web3 provider logic here:
 provider_rpc = {
     'development': 'http://localhost:9944',
     'alphanet': 'https://rpc.api.moonbase.moonbeam.network',
 }
 web3 = Web3(Web3.HTTPProvider(provider_rpc['development']))  # Change to correct network
 
+# 3. Create address variable
 account_from = {
-    'private_key': 'YOUR-PRIVATE-KEY-HERE',
-    'address': 'PUBLIC-ADDRESS-OF-PK-HERE',
+    'private_key': 'YOUR_PRIVATE_KEY_HERE',
+    'address': 'PUBLIC_ADDRESS_OF_PK_HERE',
 }
 
 print(f'Attempting to deploy from account: { account_from["address"] }')
 
+# 4. Create contract instance
 Incrementer = web3.eth.contract(abi=abi, bytecode=bytecode)
 
-construct_txn = Incrementer.constructor(5).buildTransaction(
+# 5. Build constructor tx
+construct_txn = Incrementer.constructor(5).build_transaction(
     {
         'from': account_from['address'],
-        'nonce': web3.eth.getTransactionCount(account_from['address']),
+        'nonce': web3.eth.get_transaction_count(account_from['address']),
     }
 )
 
-tx_create = web3.eth.account.signTransaction(construct_txn, account_from['private_key'])
+# 6. Sign tx with PK
+tx_create = web3.eth.account.sign_transaction(construct_txn, account_from['private_key'])
 
-tx_hash = web3.eth.sendRawTransaction(tx_create.rawTransaction)
-tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+# 7. Send tx and wait for receipt
+tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
 print(f'Contract deployed at address: { tx_receipt.contractAddress }')
--- .snippets/code/web3py-tx/transaction.py
+++ .snippets/code/web3py-tx/transaction.py
@@ -1,33 +1,42 @@
+# 1. Add imports
+from web3.gas_strategies.rpc import rpc_gas_price_strategy
 from web3 import Web3
 
+# 2. Add the Web3 provider logic here:
 provider_rpc = {
-    "development": "http://localhost:9944",
-    "alphanet": "https://rpc.api.moonbase.moonbeam.network",
+    'development': 'http://localhost:9944',
+    'alphanet': 'https://rpc.api.moonbase.moonbeam.network',
 }
-web3 = Web3(Web3.HTTPProvider(provider_rpc["development"]))  # Change to correct network
+web3 = Web3(Web3.HTTPProvider(provider_rpc['development']))  # Change to correct network
 
+# 3. Create address variables
 account_from = {
-    "private_key": "YOUR-PRIVATE-KEY-HERE",
-    "address": "PUBLIC-ADDRESS-OF-PK-HERE",
+    'private_key': 'YOUR_PRIVATE_KEY_HERE',
+    'address': 'PUBLIC_ADDRESS_OF_PK_HERE',
 }
-address_to = "ADDRESS-TO-HERE"
+address_to = 'ADDRESS_TO_HERE'
 
 print(
     f'Attempting to send transaction from { account_from["address"] } to { address_to }'
 )
 
-tx_create = web3.eth.account.signTransaction(
+# 4. Set the gas price strategy
+web3.eth.set_gas_price_strategy(rpc_gas_price_strategy)
+
+# 5. Sign tx with PK
+tx_create = web3.eth.account.sign_transaction(
     {
-        "nonce": web3.eth.getTransactionCount(account_from["address"]),
-        "gasPrice": 0,
-        "gas": 21000,
-        "to": address_to,
-        "value": web3.toWei("1", "ether"),
+        'nonce': web3.eth.get_transaction_count(account_from['address']),
+        'gasPrice': web3.eth.generate_gas_price(),
+        'gas': 21000,
+        'to': address_to,
+        'value': web3.toWei('1', 'ether'),
     },
-    account_from["private_key"],
+    account_from['private_key'],
 )
 
-tx_hash = web3.eth.sendRawTransaction(tx_create.rawTransaction)
-tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+# 6. Send tx and wait for receipt
+tx_hash = web3.eth.send_raw_transaction(tx_create.rawTransaction)
+tx_receipt = web3.eth.wait_for_transaction_receipt(tx_hash)
 
-print(f"Transaction successful with hash: { tx_receipt.transactionHash.hex() }")
+print(f'Transaction successful with hash: { tx_receipt.transactionHash.hex() }')
