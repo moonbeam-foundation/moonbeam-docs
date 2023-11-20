a8c23c7299f67a545a43434a760288f12622046c
Kevin Neilson <kevin@purestake.com>
2023-11-20 15:39:47 -0800

    Update .snippets/code/tutorials/integrations/local-subsquid/hardhat-config.js
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

--- .snippets/code/tutorials/integrations/local-subsquid/hardhat-config.js
+++ .snippets/code/tutorials/integrations/local-subsquid/hardhat-config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     dev: {
       url: 'http://127.0.0.1:9944',
-      chainId: 1281, // (hex: {{ networks.development.hex_chain_id }}),
+      chainId: 1281, // (hex: 0x501),
       accounts: [
         '0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133',
       ], // Alith's private key
