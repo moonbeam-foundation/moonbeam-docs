c2121fe5ca20437a0beee5119a2d4b8d6f81c46a
Kevin Neilson <kevin@papermoon.io>
2023-11-21 15:06:07 -0800

    run prettier

--- .snippets/code/tutorials/integrations/local-subsquid/main-with-logging.ts
+++ .snippets/code/tutorials/integrations/local-subsquid/main-with-logging.ts
@@ -1,108 +1,121 @@
-import {In} from 'typeorm'
-import {assertNotNull} from '@subsquid/evm-processor'
-import {TypeormDatabase} from '@subsquid/typeorm-store'
-import * as erc20 from './abi/erc20'
-import {Account, Transfer} from './model'
-import {Block, contractAddress, Log, Transaction, processor} from './processor'
+import { In } from 'typeorm';
+import { assertNotNull } from '@subsquid/evm-processor';
+import { TypeormDatabase } from '@subsquid/typeorm-store';
+import * as erc20 from './abi/erc20';
+import { Account, Transfer } from './model';
+import {
+  Block,
+  contractAddress,
+  Log,
+  Transaction,
+  processor,
+} from './processor';
 
-processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    ctx.log.info('Processor started');
-    let transfers: TransferEvent[] = []
+processor.run(new TypeormDatabase({ supportHotBlocks: true }), async (ctx) => {
+  ctx.log.info('Processor started');
+  let transfers: TransferEvent[] = [];
 
-    ctx.log.info(`Processing ${ctx.blocks.length} blocks`);
-    for (let block of ctx.blocks) {
-        ctx.log.debug(`Processing block number ${block.header.height}`);
-        for (let log of block.logs) {
-            ctx.log.debug(`Processing log with address ${log.address}`);
-            if (log.address === contractAddress && log.topics[0] === erc20.events.Transfer.topic) {
-                ctx.log.info(`Transfer event found in block ${block.header.height}`);
-                transfers.push(getTransfer(ctx, log))
-            }
-        }
+  ctx.log.info(`Processing ${ctx.blocks.length} blocks`);
+  for (let block of ctx.blocks) {
+    ctx.log.debug(`Processing block number ${block.header.height}`);
+    for (let log of block.logs) {
+      ctx.log.debug(`Processing log with address ${log.address}`);
+      if (
+        log.address === contractAddress &&
+        log.topics[0] === erc20.events.Transfer.topic
+      ) {
+        ctx.log.info(`Transfer event found in block ${block.header.height}`);
+        transfers.push(getTransfer(ctx, log));
+      }
     }
+  }
 
-    ctx.log.info(`Found ${transfers.length} transfers, processing...`);
-    await processTransfers(ctx, transfers)
-    ctx.log.info('Processor finished');
-})
+  ctx.log.info(`Found ${transfers.length} transfers, processing...`);
+  await processTransfers(ctx, transfers);
+  ctx.log.info('Processor finished');
+});
 
 interface TransferEvent {
-    id: string
-    block: Block
-    transaction: Transaction
-    from: string
-    to: string
-    amount: bigint
+  id: string;
+  block: Block;
+  transaction: Transaction;
+  from: string;
+  to: string;
+  amount: bigint;
 }
 
 function getTransfer(ctx: any, log: Log): TransferEvent {
-    let event = erc20.events.Transfer.decode(log)
+  let event = erc20.events.Transfer.decode(log);
 
-    let from = event.from.toLowerCase()
-    let to = event.to.toLowerCase()
-    let amount = event.value
+  let from = event.from.toLowerCase();
+  let to = event.to.toLowerCase();
+  let amount = event.value;
 
-    let transaction = assertNotNull(log.transaction, `Missing transaction`)
+  let transaction = assertNotNull(log.transaction, `Missing transaction`);
 
-    ctx.log.debug(`Decoded transfer event: from ${from} to ${to} amount ${amount.toString()}`);
-    return {
-        id: log.id,
-        block: log.block,
-        transaction,
-        from,
-        to,
-        amount,
-    }
+  ctx.log.debug(
+    `Decoded transfer event: from ${from} to ${to} amount ${amount.toString()}`
+  );
+  return {
+    id: log.id,
+    block: log.block,
+    transaction,
+    from,
+    to,
+    amount,
+  };
 }
 
 async function processTransfers(ctx: any, transfersData: TransferEvent[]) {
-    ctx.log.info('Starting to process transfer data');
-    let accountIds = new Set<string>()
-    for (let t of transfersData) {
-        accountIds.add(t.from)
-        accountIds.add(t.to)
-    }
+  ctx.log.info('Starting to process transfer data');
+  let accountIds = new Set<string>();
+  for (let t of transfersData) {
+    accountIds.add(t.from);
+    accountIds.add(t.to);
+  }
 
-    ctx.log.debug(`Fetching accounts for ${accountIds.size} addresses`);
-    let accounts = await ctx.store
-        .findBy(Account, {id: In([...accountIds])})
-        .then((q: any[]) => new Map(q.map((i: any) => [i.id, i])))
-    ctx.log.info(`Accounts fetched, processing ${transfersData.length} transfers`);
+  ctx.log.debug(`Fetching accounts for ${accountIds.size} addresses`);
+  let accounts = await ctx.store
+    .findBy(Account, { id: In([...accountIds]) })
+    .then((q: any[]) => new Map(q.map((i: any) => [i.id, i])));
+  ctx.log.info(
+    `Accounts fetched, processing ${transfersData.length} transfers`
+  );
 
-    let transfers: Transfer[] = []
+  let transfers: Transfer[] = [];
 
-    for (let t of transfersData) {
-        let {id, block, transaction, amount} = t
+  for (let t of transfersData) {
+    let { id, block, transaction, amount } = t;
 
-        let from = getAccount(accounts, t.from)
-        let to = getAccount(accounts, t.to)
+    let from = getAccount(accounts, t.from);
+    let to = getAccount(accounts, t.to);
 
-        transfers.push(
-            new Transfer({
-                id,
-                blockNumber: block.height,
-                timestamp: new Date(block.timestamp),
-                txHash: transaction.hash,
-                from,
-                to,
-                amount,
-            })
-        )
-    }
+    transfers.push(
+      new Transfer({
+        id,
+        blockNumber: block.height,
+        timestamp: new Date(block.timestamp),
+        txHash: transaction.hash,
+        from,
+        to,
+        amount,
+      })
+    );
+  }
 
-    ctx.log.debug(`Upserting ${accounts.size} accounts`);
-    await ctx.store.upsert(Array.from(accounts.values()))
-    ctx.log.debug(`Inserting ${transfers.length} transfers`);
-    await ctx.store.insert(transfers)
-    ctx.log.info('Transfer data processing completed');
+  ctx.log.debug(`Upserting ${accounts.size} accounts`);
+  await ctx.store.upsert(Array.from(accounts.values()));
+  ctx.log.debug(`Inserting ${transfers.length} transfers`);
+  await ctx.store.insert(transfers);
+  ctx.log.info('Transfer data processing completed');
 }
 
 function getAccount(m: Map<string, Account>, id: string): Account {
-    let acc = m.get(id)
-    if (acc == null) {
-        acc = new Account()
-        acc.id = id
-        m.set(id, acc)
-    }
-    return acc
+  let acc = m.get(id);
+  if (acc == null) {
+    acc = new Account();
+    acc.id = id;
+    m.set(id, acc);
+  }
+  return acc;
 }
--- .snippets/code/tutorials/integrations/local-subsquid/main.ts
+++ .snippets/code/tutorials/integrations/local-subsquid/main.ts
@@ -1,100 +1,109 @@
-import {In} from 'typeorm'
-import {assertNotNull} from '@subsquid/evm-processor'
-import {TypeormDatabase} from '@subsquid/typeorm-store'
-import * as erc20 from './abi/erc20'
-import {Account, Transfer} from './model'
-import {Block, contractAddress, Log, Transaction, processor} from './processor'
+import { In } from 'typeorm';
+import { assertNotNull } from '@subsquid/evm-processor';
+import { TypeormDatabase } from '@subsquid/typeorm-store';
+import * as erc20 from './abi/erc20';
+import { Account, Transfer } from './model';
+import {
+  Block,
+  contractAddress,
+  Log,
+  Transaction,
+  processor,
+} from './processor';
 
 // 1. Iterate through all selected blocks and look for transfer events,
 // storing the relevant events in an array of transfer events
-processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    let transfers: TransferEvent[] = []
+processor.run(new TypeormDatabase({ supportHotBlocks: true }), async (ctx) => {
+  let transfers: TransferEvent[] = [];
 
-    for (let block of ctx.blocks) {
-        for (let log of block.logs) {
-            if (log.address === contractAddress && log.topics[0] === erc20.events.Transfer.topic) {
-                transfers.push(getTransfer(ctx, log))
-            }
-        }
+  for (let block of ctx.blocks) {
+    for (let log of block.logs) {
+      if (
+        log.address === contractAddress &&
+        log.topics[0] === erc20.events.Transfer.topic
+      ) {
+        transfers.push(getTransfer(ctx, log));
+      }
     }
+  }
 
-    await processTransfers(ctx, transfers)
-})
+  await processTransfers(ctx, transfers);
+});
 
 // 2. Define an interface to hold the data from the transfer events
 interface TransferEvent {
-    id: string
-    block: Block
-    transaction: Transaction
-    from: string
-    to: string
-    amount: bigint
+  id: string;
+  block: Block;
+  transaction: Transaction;
+  from: string;
+  to: string;
+  amount: bigint;
 }
 
 // 3. Extract and decode ERC-20 transfer event data from a log entry
 function getTransfer(ctx: any, log: Log): TransferEvent {
-    let event = erc20.events.Transfer.decode(log)
+  let event = erc20.events.Transfer.decode(log);
 
-    let from = event.from.toLowerCase()
-    let to = event.to.toLowerCase()
-    let amount = event.value
+  let from = event.from.toLowerCase();
+  let to = event.to.toLowerCase();
+  let amount = event.value;
 
-    let transaction = assertNotNull(log.transaction, `Missing transaction`)
+  let transaction = assertNotNull(log.transaction, `Missing transaction`);
 
-    return {
-        id: log.id,
-        block: log.block,
-        transaction,
-        from,
-        to,
-        amount,
-    }
+  return {
+    id: log.id,
+    block: log.block,
+    transaction,
+    from,
+    to,
+    amount,
+  };
 }
 
 // 4. Enrich and insert data into typeorm database
 async function processTransfers(ctx: any, transfersData: TransferEvent[]) {
-    let accountIds = new Set<string>()
-    for (let t of transfersData) {
-        accountIds.add(t.from)
-        accountIds.add(t.to)
-    }
+  let accountIds = new Set<string>();
+  for (let t of transfersData) {
+    accountIds.add(t.from);
+    accountIds.add(t.to);
+  }
 
-    let accounts = await ctx.store
-        .findBy(Account, {id: In([...accountIds])})
-        .then((q: any[]) => new Map(q.map((i: any) => [i.id, i])))
+  let accounts = await ctx.store
+    .findBy(Account, { id: In([...accountIds]) })
+    .then((q: any[]) => new Map(q.map((i: any) => [i.id, i])));
 
-    let transfers: Transfer[] = []
+  let transfers: Transfer[] = [];
 
-    for (let t of transfersData) {
-        let {id, block, transaction, amount} = t
+  for (let t of transfersData) {
+    let { id, block, transaction, amount } = t;
 
-        let from = getAccount(accounts, t.from)
-        let to = getAccount(accounts, t.to)
+    let from = getAccount(accounts, t.from);
+    let to = getAccount(accounts, t.to);
 
-        transfers.push(
-            new Transfer({
-                id,
-                blockNumber: block.height,
-                timestamp: new Date(block.timestamp),
-                txHash: transaction.hash,
-                from,
-                to,
-                amount,
-            })
-        )
-    }
+    transfers.push(
+      new Transfer({
+        id,
+        blockNumber: block.height,
+        timestamp: new Date(block.timestamp),
+        txHash: transaction.hash,
+        from,
+        to,
+        amount,
+      })
+    );
+  }
 
-    await ctx.store.upsert(Array.from(accounts.values()))
-    await ctx.store.insert(transfers)
+  await ctx.store.upsert(Array.from(accounts.values()));
+  await ctx.store.insert(transfers);
 }
 
 // 5. Helper function to get account object
 function getAccount(m: Map<string, Account>, id: string): Account {
-    let acc = m.get(id)
-    if (acc == null) {
-        acc = new Account()
-        acc.id = id
-        m.set(id, acc)
-    }
-    return acc
+  let acc = m.get(id);
+  if (acc == null) {
+    acc = new Account();
+    acc.id = id;
+    m.set(id, acc);
+  }
+  return acc;
 }
