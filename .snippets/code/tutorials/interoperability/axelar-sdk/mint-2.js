a9c86897c34328d129035642259327fea439ccb5
Kevin Neilson <kevin@papermoon.io>
2024-02-23 11:24:50 -0800

    rev

--- .snippets/code/tutorials/interoperability/axelar-sdk/mint-2.js
+++ .snippets/code/tutorials/interoperability/axelar-sdk/mint-2.js
@@ -1,7 +1,7 @@
 const ORIGIN_CHAIN = 'moonbeam';
 const DESTINATION_CHAIN = 'ethereum-sepolia';
-const ORIGIN_CHAIN_ADDRESS = 'INSERT_CONTRACT_ADDRESS'; // This is the address
-// of CrossChainNFT printed in the console afte running deploy script.
+// Address of CrossChainNFT printed in the console after running deploy script
+const ORIGIN_CHAIN_ADDRESS = 'INSERT_CONTRACT_ADDRESS';
+// Address of AxelarAcceptEverything.sol on Sepolia, you can change this 
+// contract address to your own AxelarAcceptEverything.sol contract 
 const DESTINATION_CHAIN_ADDRESS = '0x89f801C7DB23439FDdBad4f913D788F13d1d7494'; 
-// This is set to AxelarAcceptEverything on Sepolia, you can change this 
-// contract address to your own AxelarAcceptEverything.sol contract.
