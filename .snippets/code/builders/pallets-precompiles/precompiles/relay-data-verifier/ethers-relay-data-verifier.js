460809dd6ddedaad8e437f3a0ad38e40071cbb19
Wuzhong <144415646+wuzhong-papermoon@users.noreply.github.com>
2024-04-04 16:28:06 +0800

    Edit

--- .snippets/code/builders/pallets-precompiles/precompiles/relay-data-verifier/ethers-relay-data-verifier.js
+++ .snippets/code/builders/pallets-precompiles/precompiles/relay-data-verifier/ethers-relay-data-verifier.js
@@ -1,8 +1,11 @@
-import * as fs from 'fs'; // For reading local ABI file
-import { ethers } from 'ethers'; // Import Ethers library, to interact with Moonbeam networks
-import { ApiPromise, WsProvider } from '@polkadot/api';; // Import Polkadot library, to interact with relay chain
+// For reading local ABI file
+import * as fs from 'fs'; 
+// Import Ethers library, to interact with Moonbeam networks
+import { ethers } from 'ethers'; 
+// Import Polkadot library, to interact with relay chain
+import { ApiPromise, WsProvider } from '@polkadot/api';; 
 
-var ABI= JSON.parse(fs.readFileSync('./RelayChainDataVerifierABI.json'));
+const abi = JSON.parse(fs.readFileSync('./RelayChainDataVerifierABI.json'));
 const privateKey = 'INSERT_PRIVATE_KEY';
 const precompileAddress = '0x0000000000000000000000000000000000000819';
 const moonbeamURL = 'https://rpc.api.moonbase.moonbeam.network';
@@ -12,29 +15,31 @@ const relayURL = 'wss://frag-moonbase-relay-rpc-ws.g.moonbase.moonbeam.network';
 const provider = new ethers.JsonRpcProvider(moonbeamURL);
 const signer = new ethers.Wallet(privateKey, provider);
 const precompileContract = new ethers.Contract(
-    precompileAddress,
-    ABI,
-    signer
+  precompileAddress,
+  abi,
+  signer
 );
 
-// Create provider for relay chain
-const wsProvider = new WsProvider(relayURL);
-const api = await ApiPromise.create({ provider: wsProvider });
+async function run() {
+  // Create provider for relay chain
+  const wsProvider = new WsProvider(relayURL);
+  const api = await ApiPromise.create({ provider: wsProvider });
 
-async function run(){
   // Get the storage key for a random account on relay chain
-  let key = await api.query.system.account.key(
+  const key = api.query.system.account.key(
     '5CBATpb3yvEM4mhX9Dw3tyuqiWKhq9YBG6ugSbodRUSbodoU'
   )
-  // Find the latest available block number(relay chain) from moonbeam
-  let blockNum = await precompileContract.latestRelayBlockNumber()
+  // Find the latest available relay chain block number
+  const blockNum = await precompileContract.latestRelayBlockNumber()
 
   // Get the blockHash and storage proof from relay chain
-  let blockHash = await api.rpc.chain.getBlockHash(blockNum);
-  let proof = await api.rpc.state.getReadProof([key],blockHash);
-  
+  const blockHash = await api.rpc.chain.getBlockHash(blockNum);
+  const proof = await api.rpc.state.getReadProof([key], blockHash);
+
   // This tx will be rejected if the verification failed
-  await precompileContract.verifyEntry(blockNum, proof, key)
+  let receipt = await precompileContract.verifyEntry(blockNum, proof, key)
+  await receipt.wait();
+  console.log(receipt.hash)
 }
 
 await run();
--- .snippets/code/builders/pallets-precompiles/precompiles/relay-data-verifier/web3js-relay-data-verifier.js
+++ .snippets/code/builders/pallets-precompiles/precompiles/relay-data-verifier/web3js-relay-data-verifier.js
@@ -1,8 +1,12 @@
-import * as fs from 'fs'; // For reading local ABI file
-import { Web3 } from 'web3'; // Import web3js library, to interact with Moonbeam networks
-import { ApiPromise, WsProvider } from '@polkadot/api';; // Import Polkadot library, to interact with relay chain
+// For reading local ABI file
+import * as fs from 'fs';
+// Import web3js library, to interact with Moonbeam networks
+import { Web3 } from 'web3';
+// Import Polkadot library, to interact with relay chain
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { constants } from 'os';
 
-var ABI= JSON.parse(fs.readFileSync('./RelayChainDataVerifierABI.json'));
+const abi = JSON.parse(fs.readFileSync('./RelayChainDataVerifierABI.json'));
 const privateKey = 'INSERT_PRIVATE_KEY';
 const precompileAddress = '0x0000000000000000000000000000000000000819';
 const moonbeamURL = 'https://rpc.api.moonbase.moonbeam.network';
@@ -10,17 +14,17 @@ const relayURL = 'wss://frag-moonbase-relay-rpc-ws.g.moonbase.moonbeam.network';
 
 // Create Web3js provider and signer
 const web3 = new Web3(moonbeamURL);
-const precompileContract = new web3.eth.Contract(ABI, precompileAddress)
-
-// Create provider for relay chain
-const wsProvider = new WsProvider(relayURL);
-const api = await ApiPromise.create({ provider: wsProvider });
+const precompileContract = new web3.eth.Contract(abi, precompileAddress)
 
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 
-async function run(){
+async function run() {
+    // Create provider for relay chain
+    const wsProvider = new WsProvider(relayURL);
+    const api = await ApiPromise.create({ provider: wsProvider });
+
     // Get the storage key for a random account on relay chain
-    const key = await api.query.system.account.key(
+    const key = api.query.system.account.key(
         '5CBATpb3yvEM4mhX9Dw3tyuqiWKhq9YBG6ugSbodRUSbodoU'
     )
     // Find the latest available block number(relay chain) from moonbeam
@@ -28,8 +32,8 @@ async function run(){
 
     // Get the blockHash and storage proof from relay chain
     const blockHash = await api.rpc.chain.getBlockHash(blockNum);
-    const proof = await api.rpc.state.getReadProof([key],blockHash);
-  
+    const proof = await api.rpc.state.getReadProof([key], blockHash);
+
     const callObject = {
         to: precompileAddress,
         data: precompileContract.methods.verifyEntry(blockNum, proof, key).encodeABI(),
@@ -37,12 +41,11 @@ async function run(){
         gasPrice: await web3.eth.getGasPrice(),
         nonce: await web3.eth.getTransactionCount(account.address),
     }
-  
+
     // This tx will be rejected if the verification failed
     const tx = await web3.eth.accounts.signTransaction(callObject, account.privateKey);
-    web3.eth.sendSignedTransaction(tx.rawTransaction)
-    .on('receipt', console.log)
-    .on('error', function(error){ console.log(error) });
+    const receipt = await web3.eth.sendSignedTransaction(tx.rawTransaction)
+    console.log(receipt.transactionHash)
 
 }
 
