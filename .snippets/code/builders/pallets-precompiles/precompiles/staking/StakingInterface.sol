93f8a4c8389a750b0172dcd8bf3af32b8b00bf05
Kevin Neilson <kevin@purestake.com>
2024-04-11 14:18:20 -0700

    Update .snippets/code/builders/pallets-precompiles/precompiles/staking/StakingInterface.sol
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

--- .snippets/code/builders/pallets-precompiles/precompiles/staking/StakingInterface.sol
+++ .snippets/code/builders/pallets-precompiles/precompiles/staking/StakingInterface.sol
@@ -11,7 +11,7 @@ ParachainStaking constant PARACHAIN_STAKING_CONTRACT = ParachainStaking(
 
 /// @author The Moonbeam Team
 /// @title Pallet Parachain Staking Interface
-/// @dev The interface through which solidity contracts will interact with Parachain Staking
+/// @dev The interface through which Solidity contracts will interact with Parachain Staking
 /// We follow this same interface including four-byte function selectors, in the precompile that
 /// wraps the pallet
 /// @custom:address 0x0000000000000000000000000000000000000800
