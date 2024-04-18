63caf7eee5140932ae1790f873834c23b1c1e21c
Kevin Neilson <kevin@purestake.com>
2024-04-17 19:55:52 -0700

    Update .snippets/code/builders/build/eth-api/dev-env/openzeppelin/ERC721.sol
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

--- .snippets/code/builders/build/eth-api/dev-env/openzeppelin/ERC721.sol
+++ .snippets/code/builders/build/eth-api/dev-env/openzeppelin/ERC721.sol
@@ -21,8 +21,7 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
         _safeMint(to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity.
-
+    // The following functions are overrides required by Solidity
     function _update(address to, uint256 tokenId, address auth)
         internal
         override(ERC721, ERC721Enumerable)
