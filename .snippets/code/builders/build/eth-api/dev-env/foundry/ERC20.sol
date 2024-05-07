01702e2828371be5fb997a0bff66e44c16a8a57d
Kevin Neilson <kevin@purestake.com>
2024-05-07 11:14:02 -0700

    Update .snippets/code/builders/build/eth-api/dev-env/foundry/ERC20.sol
    Co-authored-by: Erin Shaben <eshaben@icloud.com>

--- .snippets/code/builders/build/eth-api/dev-env/foundry/ERC20.sol
+++ .snippets/code/builders/build/eth-api/dev-env/foundry/ERC20.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // This ERC-20 contract mints the specified amount of tokens to the contract creator
 contract MyToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("MyToken", "MYTOK") {
-    _mint(msg.sender, initialSupply);
-  }
+    constructor(uint256 initialSupply) ERC20("MyToken", "MYTOK") {
+        _mint(msg.sender, initialSupply);
+    }
 }
