6751b7fd564e0f40d50ee9adb653780413765857
Taylor Lucero <telucero@gmail.com>
2025-05-07 17:26:50 +0900

    prettier formatting

--- js/cookbookEventHandler.js
+++ js/cookbookEventHandler.js
@@ -1,8 +1,7 @@
-
-    document.body.addEventListener('click', (event) => {
-      if (event.target.tagName === 'ASK-COOKBOOK') {
-        event.target.addEventListener('keydown', (event) => {
-          event.stopPropagation()
-        })
-      }
+document.body.addEventListener('click', (event) => {
+  if (event.target.tagName === 'ASK-COOKBOOK') {
+    event.target.addEventListener('keydown', (event) => {
+      event.stopPropagation();
     });
+  }
+});
