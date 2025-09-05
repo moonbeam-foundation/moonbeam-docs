4bdda96871c2522fd12ef9296566c2ee4c253c1a
Aljosa Makevic <aljosa100@gmail.com>
2025-09-05 16:35:06 +0200

    commiting the rest of the files

--- .venv/lib/python3.11/site-packages/backrefs/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Backrefs package."""
-from .__meta__ import __version__, __version_info__  # noqa: F401
--- .venv/lib/python3.11/site-packages/backrefs/_bregex_typing.py
+++
@@ -1,6 +0,0 @@
-"""Typing objects for the Regex library."""
-import regex  # type: ignore[import]
-from typing import _alias  # type: ignore[attr-defined]
-
-Pattern = _alias(type(regex.compile('')), 1)
-Match = _alias(type(regex.compile('').match('')), 1)
