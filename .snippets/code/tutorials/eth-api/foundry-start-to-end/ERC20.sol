ac44276249241e96a5936eeb76f4f9307d58ab24
Kevin Neilson <kevin@papermoon.io>
2025-10-08 08:40:42 -0700

    Update Solc Versions (#1239)
    * update solc
    
    * feedback updates and llms
    
    * couple more changes
    
    * llms
    
    * revise remix
    
    * revisions
    
    * llms

--- .snippets/code/tutorials/eth-api/foundry-start-to-end/ERC20.sol
+++ .snippets/code/tutorials/eth-api/foundry-start-to-end/ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.30;
 
 // Import OpenZeppelin Contract
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
