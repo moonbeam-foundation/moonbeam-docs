336a02b6d3c641b66416b1d544a256c6db6d0e1d
Kevin Neilson <kevin@papermoon.io>
2025-12-06 16:08:04 -0800

    Remove eth-api tutorial pages

--- .snippets/code/tutorials/eth-api/foundry-start-to-end/Fuzz-test.sol
+++
@@ -1,7 +0,0 @@
-// Fuzz tests for success upon minting tokens one ether or below
-function testMintOneEtherOrBelow(uint256 amountToMint) public {
-    vm.assume(amountToMint <= 1 ether);
-
-    token.mint(amountToMint, msg.sender);
-    assertEq(token.balanceOf(msg.sender), amountToMint);
-}
