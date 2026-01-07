21596ad61d2fce4abf831b7b0c70547019858027
Kevin Neilson <kevin@papermoon.io>
2026-01-07 15:39:24 -0500

    Decom Web3Js Broken Links and Revise  (#1361)
    * decom web3js stuff
    
    * revisions
    
    * rev
    
    * run llms

--- .snippets/code/builders/ethereum/json-rpc/pubsub/subscribe-to-event-logs.js
+++ .snippets/code/builders/ethereum/json-rpc/pubsub/subscribe-to-event-logs.js
@@ -1,28 +1,38 @@
-const { Web3 } = require('web3');
-const web3 = new Web3('wss://wss.api.moonbase.moonbeam.network');
+const { ethers } = require('ethers');
+
+const provider = new ethers.WebSocketProvider(
+  'wss://wss.api.moonbase.moonbeam.network'
+);
+
+const tokenAddress = 'INSERT_CONTRACT_ADDRESS';
+const abi = [
+  'event Transfer(address indexed from, address indexed to, uint256 value)',
+];
+const iface = new ethers.Interface(abi);
+const transferTopic = ethers.id('Transfer(address,address,uint256)');
+
+const filter = {
+  address: tokenAddress,
+  topics: [transferTopic],
+};
 
 const main = async () => {
-  const subscsription = await web3.eth.subscribe(
-    'logs',
-    {
-      address: 'INSERT_CONTRACT_ADDRESS',
-      topics: [
-        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-      ],
-    },
-    (error, result) => {
-      if (error) console.error(error);
-    }
-  );
-
   console.log('🕔 Subscription set up. Waiting for new logs');
 
-  subscsription.on('connected', function (subscriptionId) {
-    console.log(subscriptionId);
-  });
-  subscsription.on('data', function (log) {
-    console.log(log);
+  provider.on(filter, (log) => {
+    const parsed = iface.parseLog(log);
+
+    console.log({
+      from: parsed.args.from,
+      to: parsed.args.to,
+      value: parsed.args.value.toString(),
+      blockNumber: log.blockNumber,
+      txHash: log.transactionHash,
+    });
   });
 };
 
-main();
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
