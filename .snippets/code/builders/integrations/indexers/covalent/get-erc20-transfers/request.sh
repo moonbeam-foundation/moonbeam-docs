e19b3670aa575e0fcc5744e2816106d59b29bc31
Kevin Neilson <kevin@papermoon.io>
2026-02-10 12:12:37 -0800

    clean up old snippet files

--- .snippets/code/builders/integrations/indexers/covalent/get-erc20-transfers/request.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-  --url 'https://api.covalenthq.com/v1/moonbeam-moonbase-alpha/address/0x3B939FeaD1557C741Ff06492FD0127bd287A421e/transfers_v2/?contract-address=0x37822de108AFFdd5cDCFDaAa2E32756Da284DB85' \
-  --header 'Authorization: Bearer INSERT_API_KEY'
--- .snippets/code/builders/integrations/indexers/covalent/transaction-summary/request.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-  --url https://api.covalenthq.com/v1/moonbeam-moonbase-alpha/address/0x3B939FeaD1557C741Ff06492FD0127bd287A421e/transactions_summary/ \
-  --header 'Authorization: Bearer INSERT_API_KEY'
