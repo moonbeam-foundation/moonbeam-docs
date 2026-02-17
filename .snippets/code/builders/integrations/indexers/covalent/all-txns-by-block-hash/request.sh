9b76d116c8f75561b2b0666793ae5b19e9ec19a6
Kevin Neilson <kevin@papermoon.io>
2026-02-17 01:19:53 -0800

    GoldRush API (Formerly Covalent) Updates (#1388)
    * covalent updates
    
    * clean up old snippet files
    
    * rev
    
    * rev
    
    * revisions

--- .snippets/code/builders/integrations/indexers/covalent/all-txns-by-block-hash/request.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-  --url https://api.covalenthq.com/v1/moonbeam-moonbase-alpha/block_hash/0x2a9cda3cfd23dffbe064932991568cae601d178717743eabc326222123d7ad44/transactions_v3/ \
-  --header 'Authorization: Bearer INSERT_API_KEY'
--- .snippets/code/builders/integrations/indexers/covalent/nft-approvals/request.sh
+++
@@ -1,3 +0,0 @@
-curl --request GET \
-  --url https://api.covalenthq.com/v1/moonbeam-moonbase-alpha/nft/approvals/0xe469220D444D4c30361F1416082EA4Bf3f2A55B3/ \
-  --header 'Authorization: Bearer INSERT_API_KEY'
